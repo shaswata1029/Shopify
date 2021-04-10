25461bf83ed71cc16360b7a69a7bba5a8b2a3cdf
Shaswata Raha <shaswata1029@gmail.com>
2021-04-10 11:34:34 +0530

    added all routes related to Orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,6 +3,7 @@ const Order = require("../models/order");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const order = require("../models/order");
 
 // Create a new Order
 module.exports.newOrder = catchAsyncErrors(async (req, res, next) => {
@@ -72,5 +73,86 @@ module.exports.userOrders = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     orders,
+  });
+});
+
+// Get all orders from database(for admin)
+
+module.exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find().populate("user", "name email");
+
+  let totalAmount = 0;
+  orders.forEach((order) => (totalAmount += order.totalPrice));
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+// Update/Process the order (for admin)
+
+module.exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler(`No Order with this id`, 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler(`You have already delivered this order`, 400));
+  }
+
+  // console.log(order);
+
+  order.orderItems.forEach(async (item) => {
+    await upDateStock(item.product, item.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Order processed successfully",
+  });
+});
+
+async function upDateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock = product.stock - quantity;
+  await product.save({ validateBeforeSave: false });
+}
+
+// Delete order(for admin)
+
+module.exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler(`No Order with this id`, 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler(`Delivered order cannot be deleted`, 400));
+  }
+
+  // console.log(order);
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Order deleted successfully",
   });
 });
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -6,5 +6,24 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 router.post("/new", isAuthenticatedUser, orderController.newOrder);
 router.get("/user/:id", isAuthenticatedUser, orderController.getSingleOrder);
 router.get("/user", isAuthenticatedUser, orderController.userOrders);
+router.get(
+  "/admin",
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  orderController.allOrders
+);
+
+router
+  .route("/admin/:id")
+  .put(
+    isAuthenticatedUser,
+    authorizeRoles("admin"),
+    orderController.updateOrder
+  )
+  .delete(
+    isAuthenticatedUser,
+    authorizeRoles("admin"),
+    orderController.deleteOrder
+  );
 
 module.exports = router;
